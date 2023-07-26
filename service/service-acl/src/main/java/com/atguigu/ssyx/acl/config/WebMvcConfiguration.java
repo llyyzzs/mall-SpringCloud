354d6f380fb9859d5d7a6f41bd202f64d478e2da
lyz-vue-springboot <2736836265@qq.com>
2023-07-26 12:41:25 +0800

    尚硅谷

---
+++ service/service-acl/src/main/java/com/atguigu/ssyx/acl/config/WebMvcConfiguration.java
@@ -0,0 +1,25 @@
+package com.atguigu.ssyx.acl.config;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+@Configuration
+@Slf4j
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        log.info("111");
+//        registry.addMapping("/**") // 允许跨域访问的路径
+//                .allowedOrigins("*")// 允许跨域访问的源
+//                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE") // 允许请求方法
+//                .maxAge(86400) // 预检间隔时间
+//                .allowedHeaders("*") // 允许头部设置
+//                .allowCredentials(true); // 是否发送cookie
+//    }
+}
